c19d185bbce97ac7b2dab10ca8958f100f4b12c9
Francisco J.H. Heras <fjhheras@gmail.com>
2018-03-24 10:54:33 +0000

    Runs (slow) and gives results in the right ball park

--- trajectorytools/socialcontext/leadership.py
+++ trajectorytools/socialcontext/leadership.py
@@ -1,3 +1,5 @@
+import numpy as np
+import trajectorytools as tt
 from .socialcontext import restrict
 
 def restrict_with_delay(data, indices, individual=None, delay=0):
@@ -6,7 +8,12 @@ def restrict_with_delay(data, indices, individual=None, delay=0):
     indices accordingly 
     '''
     delayed_data = data[delay:]
-    restricted_indices = indices[:-delay]
+    if delay == 0:
+        restricted_indices = indices
+    elif delay > 0:
+        restricted_indices = indices[:-delay]
+    else:
+        raise NotImplementedError
     return restrict(delayed_data, restricted_indices, individual=individual)
 
 def sweep_delays(data, indices, max_delay):
@@ -31,7 +38,7 @@ def sweep_delayed_orientation_with_neighbours(orientation, indices, max_delay):
     total_time_steps = orientation.shape[0] - max_delay
     sweep_delay_e = sweep_delays(orientation, indices, max_delay)
     # dimensions: delay x time x num_individuals x num nb x 2
-    sweep_delay_P = tt.polarization(sweep_delay_e)
+    sweep_delay_P = tt.collective.polarization(sweep_delay_e)
     # dimensions: delay x time x num_individuals x 2
     restricted_orientation = orientation[:total_time_steps]
     projected_orientation = np.einsum('...j,i...j->i...',restricted_orientation, sweep_delay_P) 
