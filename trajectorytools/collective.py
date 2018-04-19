95faef63755c1b2a083a38a0e435a1609fe1cf7c
Francisco J Hernandez Heras <fjhheras@gmail.com>
2018-04-19 15:34:18 +0100

    Cleaning up

--- trajectorytools/collective.py
+++ trajectorytools/collective.py
@@ -1,28 +1,29 @@
 import numpy as np
 import trajectorytools as tt
 
+
 def average_across_individuals(s):
     """ Averages along the penultimate dimension
     """
-    return np.mean(s, axis = -2)
+    return np.mean(s, axis=-2)
+
 
 def sum_across_individuals(s):
     """ Sums along the penultimate dimension
     """
-    return np.sum(s, axis = -2)
+    return np.sum(s, axis=-2)
+
 
 def polarization(v):
     """ Calculates (the normalised) polarization of the vectors
     """
-    norm_v = tt.norm(v, keepdims = True)
+    norm_v = tt.norm(v, keepdims=True)
     return sum_across_individuals(v) / sum_across_individuals(norm_v)
 
-def angular_momentum(v,s, center = np.array([0,0])):
-    if len(center.shape) == 1: #A point
+
+def angular_momentum(v, s, center=np.array([0, 0])):
+    if len(center.shape) == 1:  # A point
         delta_s = s - center[np.newaxis, np.newaxis, ...]
-    if len(center.shape) == 2: #A trajectory
+    if len(center.shape) == 2:  # A trajectory
         delta_s = s - np.expand_dims(center, 1)
-    return np.sum(tt.cross2D(v,delta_s), axis = -1)
-
-
-
+    return np.sum(tt.cross2D(v, delta_s), axis=-1)
--- trajectorytools/trajectories.py
+++ trajectorytools/trajectories.py
@@ -2,45 +2,48 @@ from argparse import Namespace
 import numpy as np
 import trajectorytools as tt
 
+
 def calculate_center_of_mass(trajectories):
     center_of_mass = Namespace()
     center_of_mass_dict = vars(center_of_mass)
     trajectories_dict = vars(trajectories)
-    center_of_mass_dict.update({k: np.nanmean(v, axis = 1) for k,v in trajectories_dict.items()})
+    center_of_mass_dict.update(
+            {k: np.nanmean(v, axis=1) for k, v in trajectories_dict.items()})
     return center_of_mass
+
 
 class Trajectories():
     def __init__(self, trajectories):
         self.trajectories = trajectories
         self.center_of_mass = calculate_center_of_mass(trajectories)
         self.__dict__.update(vars(self.trajectories))
-    
+
     def __getitem__(self, val):
         view_trajectories = Namespace()
-        vars(view_trajectories).update({k: v[val] for k,v in vars(self.trajectories).items()})
+        vars(view_trajectories).update(
+                {k: v[val] for k, v in vars(self.trajectories).items()})
         return Trajectories(view_trajectories)
-    
-    def view(self, start = None, end = None):
-        return self[slice(start,end)]
-        #view_trajectories = Namespace()
-        #vars(view_trajectories).update({k: v[start:end] for k,v in vars(self.trajectories).items()})
-        #return Trajectories(view_trajectories)
+
+    def view(self, start=None, end=None):
+        return self[slice(start, end)]
 
     @classmethod
-    def from_idtracker(cls, trajectories_path, interpolate_nans = True, dtype = np.float64):
-        trajectories_dict = np.load(trajectories_path, encoding = 'latin1').item()
-        ### Bring here the properties that we need from the dictionary
-        t = trajectories_dict['trajectories'].astype(dtype)
-        return cls.from_positions(t, interpolate_nans = interpolate_nans)
+    def from_idtracker(cls, trajectories_path,
+                       interpolate_nans=True, dtype=np.float64):
+        traj_dict = np.load(trajectories_path, encoding='latin1').item()
+        # Bring here the properties that we need from the dictionary
+        t = traj_dict['trajectories'].astype(dtype)
+        return cls.from_positions(t, interpolate_nans=interpolate_nans)
 
     @classmethod
-    def from_positions(cls, t, interpolate_nans = True):
+    def from_positions(cls, t, interpolate_nans=True):
         trajectories = Namespace()
         trajectories.raw = t.copy()
         tt.normalise_trajectories(t)
         if interpolate_nans:
             tt.interpolate_nans(t)
-        [trajectories.s, trajectories.v, trajectories.a] = tt.smooth_several(t, derivatives=[0,1,2])
+        [trajectories.s, trajectories.v, trajectories.a] = tt.smooth_several(
+                t, derivatives=[0, 1, 2])
         trajectories.speed = tt.norm(trajectories.v)
         trajectories.acceleration = tt.norm(trajectories.a)
         trajectories.distance_to_center = tt.norm(trajectories.s)
@@ -51,13 +54,17 @@ class Trajectories():
     @property
     def number_of_frames(self):
         return self.s.shape[0]
+
     @property
     def number_of_individuals(self):
         return self.s.shape[1]
+
     @property
-    def identity_labels(self): #Placeholder, in case in the future labels are explicitly given
+    def identity_labels(self):
+        # Placeholder, in case in the future labels are explicitly given
         return np.arange(self.number_of_individuals)
+
     @property
     def identities_array(self):
-        ones = np.ones(self.raw.shape[:-1] ,dtype = np.int)
-        return np.einsum('ij,j->ij',ones, self.identity_labels)
+        ones = np.ones(self.raw.shape[:-1], dtype=np.int)
+        return np.einsum('ij,j->ij', ones, self.identity_labels)
