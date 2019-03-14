b5fcb0d4141cc40ab9601277b2e6730f8a18e76d
Francisco J Hernandez Heras <fjhheras@gmail.com>
2019-03-14 13:42:25 +0000

    Faster algorithm for bouts

--- trajectorytools/fish_bouts/fish_bouts.py
+++ trajectorytools/fish_bouts/fish_bouts.py
@@ -1,45 +1,27 @@
 import numpy as np
 from scipy import signal
 
+def get_bouts(tr, **kwargs):
+    """Obtain bouts start and peak for all individuals
 
-def get_bouts(tr, prominence, distance):
+    :param tr: trajectorytools Trajectories instance
+    :param **kwargs: named arguments passed to scipy.signal.find_peaks
+    """
     all_starting_bouts = []
     all_bout_peaks = []
     for focal in range(tr.number_of_individuals):
-        print("Computing bouts for individual {:.0f}".format(focal))
-        # Find local minima an maxima
-        min_frames = signal.find_peaks(-tr.speed[:, focal],
-                                       prominence=prominence,
-                                       distance=distance)[0]
-        max_frames = signal.find_peaks(tr.speed[:, focal],
-                                       prominence=prominence,
-                                       distance=distance)[0]
-        # Compute bouts by pairs of minimums (start) and maximums peaks
-        bouts = []
-        bout = []
-        for frame in range(tr.number_of_frames):
-            if not np.isnan(tr.s[frame, focal, 0]):
-                if frame in min_frames:
-                    # The starting of the bout is the minimum closer to the
-                    # next maximum
-                    if len(bout) == 0:
-                        bout.append(frame)
-                    elif len(bout) == 1:
-                        bout = []
-                        bout.append(frame)
-                elif frame in max_frames:
-                    # The peak of the bout is the first maximum after the last
-                    # minimum
-                    if len(bout) == 1:
-                        bout.append(frame)
-                        bouts.append(bout)
-                        bout = []
-            else:
-                print('pass')
-                bout = []
+        #print("Computing bouts for individual {:.0f}".format(focal))
+        # Find local minima and maxima
+        min_frames_ = signal.find_peaks(-tr.speed[:, focal], **kwargs)[0]
+        max_frames_ = signal.find_peaks(tr.speed[:, focal], **kwargs)[0]
+        min_frames = [f for f in min_frames_ if not np.isnan(tr.s[f, focal, 0])]
+        max_frames = [f for f in max_frames_ if not np.isnan(tr.s[f, focal, 0])]
+        frames = min_frames + max_frames
+        frameismax = [False]*len(min_frames) + [True]*len(max_frames)
+        ordered_frames, ordered_ismax = zip(*sorted(zip(frames, frameismax)))
+        bouts = [ordered_frames[i:i+2] for i in range(len(ordered_frames)-1)
+                 if not ordered_ismax[i] and ordered_ismax[i+1]]
         starting_bouts, bout_peaks = zip(*bouts)
-        starting_bouts = np.asarray(starting_bouts)
-        bout_peaks = np.asarray(bout_peaks)
-        all_starting_bouts.append(starting_bouts)
-        all_bout_peaks.append(bout_peaks)
+        all_starting_bouts.append(np.asarray(starting_bouts))
+        all_bout_peaks.append(np.asarray(bout_peaks))
     return all_starting_bouts, all_bout_peaks
