fix indexing in signed angle computation

trajectorytools/geometry.py
@@ -76,7 +76,7 @@ def signed_angle_between_vectors(x, y):
     '''A different algorithms to calculate angle between vectors,
     between -pi and pi radians
     '''
-    angle = np.arctan2(y[1], y[0]) - np.arctan2(x[1], x[0])
+    angle = np.arctan2(y[..., 1], y[..., 0]) - np.arctan2(x[..., 1], x[..., 0])
     return (angle + np.pi)%(2*np.pi) - np.pi
 
 def _ey_to_ex(e_):
@@ -86,4 +86,3 @@ def _ey_to_ex(e_):
     ex[...,1] = -e_[...,0]
     ex[...,0] = e_[...,1]
     return ex
-