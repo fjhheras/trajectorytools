b8dc98918e50b9afc44372173cd18ba5636cf9ac
Francisco J Hernandez Heras <fjhheras@gmail.com>
2019-03-25 10:49:37 +0000

    More bout statistics

--- trajectorytools/fish_bouts/fish_bouts.py
+++ trajectorytools/fish_bouts/fish_bouts.py
@@ -1,41 +1,60 @@
 import trajectorytools as tt
 
+def bout_statistics():
 
-def bout_latency(tr, bout, focal):
-    return bout[2]-bout[0]
+    def latency(tr, bout, focal):
+        return bout[2]-bout[0]
 
+    def acceleration_time(tr, bout, focal):
+        return bout[1]-bout[0]
 
-def bout_acceleration_time(tr, bout, focal):
-    return bout[1]-bout[0]
+    def gliding_time(tr, bout, focal):
+        """
+        It can only be interpreted as gliding time if the end of the current bout
+        coincides with the beginning of the next bout
+        """
+        return bout[2]-bout[1]
 
+    def location_start(tr, bout, focal):
+        return tr.s[bout[0], focal]
 
-def bout_gliding_time(tr, bout, focal):
-    """
-    It can only be interpreted as gliding time if the end of the current bout
-    coincides with the beginning of the next bout
-    """
-    return bout[2]-bout[1]
+    def location_end(tr, bout, focal):
+        return tr.s[bout[1], focal]
 
+    def location_end_gliding(tr, bout, focal):
+        return tr.s[bout[2], focal]
 
-def bout_displacement(tr, bout, focal):
-    return tt.norm(tr.s[bout[2], focal] - tr.s[bout[0], focal])
+    def displacement(tr, bout, focal):
+        return tt.norm(tr.s[bout[1], focal] - tr.s[bout[0], focal])
 
+    def displacement_with_gliding(tr, bout, focal):
+        return tt.norm(tr.s[bout[2], focal] - tr.s[bout[0], focal])
 
-def bout_turning_angle(tr, bout, focal):
-    return tt.signed_angle_between_vectors(tr.v[bout[2]], tr.v[bout[0]])
+    def turning_angle(tr, bout, focal):
+        return tt.signed_angle_between_vectors(tr.v[bout[1], focal], tr.v[bout[0], focal])
 
+    def turning_angle_with_gliding(tr, bout, focal):
+        return tt.signed_angle_between_vectors(tr.v[bout[2], focal], tr.v[bout[0], focal])
+
+    def turning_angle_old(tr, bout, focal):
+        return tt.angle_between_vectors(tr.v[bout[1], focal], tr.v[bout[0], focal])
+
+    return [value for key, value in locals().items()
+            if callable(value) and not key.startswith('__')]
 
 def compute_bouts_parameters(tr, bouts, focal):
-    variables = [bout_latency, bout_acceleration_time, bout_gliding_time,
-                 bout_displacement, bout_turning_angle]
+    #variables = [bout_latency, bout_acceleration_time, bout_gliding_time,
+    #            bout_displacement, bout_turning_angle, bout_turning_angle_old]
+    variables = bout_statistics()
     bouts_dict = {var.__name__: [var(tr, bout, focal) for bout in bouts]
-                  for var in variables}
+                for var in variables}
     bouts_dict['bouts'] = bouts
     return bouts_dict
 
 
-def get_bouts_parameters(tr):
-    all_bouts = tr.get_bouts(prominence=(0.002, None), distance=3)
+def get_bouts_parameters(tr, **kwargs):
+    #all_bouts = tr.get_bouts(prominence=(0.002, None), distance=3)
+    all_bouts = tr.get_bouts(**kwargs)
     indiv_bouts = [compute_bouts_parameters(tr, all_bouts[focal], focal)
                    for focal in range(tr.number_of_individuals)]
     return indiv_bouts
