5f1719093acd714bc62b325dc38c6ef5fcc5430f
F.J.H. Heras <fjhheras@gmail.com>
2020-05-11 20:05:14 +0100

    Preparing requirements and setup

--- setup.py
+++ setup.py
@@ -11,5 +11,16 @@ setup(name='trajectorytools',
       packages=['trajectorytools'],
       package_data={'trajectorytools': ['data/*.npy']},
       include_package_data=True,
-      zip_safe=False)
+      zip_safe=False,
+
+      classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities'
+      ],
+
+      )
 
