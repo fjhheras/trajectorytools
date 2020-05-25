c15eaae0ef91df9f9afe393a76aa29d064abe863
F.J.H. Heras <fjhheras@gmail.com>
2020-05-25 16:58:02 +0100

    Changes in category in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(name='trajectorytools',
-      version='0.3-alpha.2',
+      version='0.3-alpha.3',
       description='A tool to study 2D trajectories',
       long_description=long_description,
       url='http://github.com/fjhheras/trajectorytools',
@@ -12,10 +12,10 @@ setup(name='trajectorytools',
       author_email='fjhheras@gmail.com',
       license='GPL',
 
-      install_requires = ['MiniballCpp',
-                          'matplotlib',
-                          'scikit-learn',
-                          'scipy'],
+      install_requires=['MiniballCpp',
+                        'matplotlib',
+                        'scikit-learn',
+                        'scipy'],
 
       packages=['trajectorytools'],
       package_data={'trajectorytools': ['data/*.npy']},
@@ -23,13 +23,12 @@ setup(name='trajectorytools',
       zip_safe=False,
 
       classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Utilities'
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Physics',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Utilities'
       ],
 
       )
-
