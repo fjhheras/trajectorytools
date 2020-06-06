a74c2ad886f6c023ef4592f846f52bb9444614de
F.J.H. Heras <fjhheras@gmail.com>
2020-06-06 09:11:35 +0100

    Changing threshold so test does not fail by chance

--- tests/interpolate_test.py
+++ tests/interpolate_test.py
@@ -20,9 +20,9 @@ class TestCircle():
     def test_consistency(self, func_find_circle):
         center_x, center_y, radius = func_find_circle(self.t)
         # It is possible but highly improbable that this test fails by chance
-        assert pytest.approx(center_x, 1e-3) == self.center[0]
-        assert pytest.approx(center_y, 1e-3) == self.center[1]
-        assert pytest.approx(radius, 1e-3) == self.R
+        assert pytest.approx(center_x, 1e-2) == self.center[0]
+        assert pytest.approx(center_y, 1e-2) == self.center[1]
+        assert pytest.approx(radius, 1e-2) == self.R
 
     @pytest.mark.parametrize("func_find_circle", [find_enclosing_circle,
                                                   find_enclosing_circle_simple])
@@ -33,9 +33,9 @@ class TestCircle():
               np.random.randint(0, 10), :] = np.nan
         center_x, center_y, radius = func_find_circle(t)
         # It is possible but highly improbable that this test fails by chance
-        assert pytest.approx(center_x, 1e-3) == self.center[0]
-        assert pytest.approx(center_y, 1e-3) == self.center[1]
-        assert pytest.approx(radius, 1e-3) == self.R
+        assert pytest.approx(center_x, 1e-2) == self.center[0]
+        assert pytest.approx(center_y, 1e-2) == self.center[1]
+        assert pytest.approx(radius, 1e-2) == self.R
 
     @pytest.fixture(autouse=True)
     def run_around_tests(self):
