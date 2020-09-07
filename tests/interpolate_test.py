442823457fd064cc730c0460808e90bc78e88867
F.J.H. Heras <fjhheras@gmail.com>
2020-09-07 10:49:56 +0100

    test failed sometimes

--- tests/interpolate_test.py
+++ tests/interpolate_test.py
@@ -15,9 +15,9 @@ from trajectorytools.interpolate import (
 
 class TestCircle:
     def setup_class(self):
-        angles = np.random.rand(50, 10) * 2 * np.pi
-        self.center = np.random.randn(2)
-        self.R = 1 + np.random.rand(1)
+        angles = np.random.rand(2000, 10) * 2 * np.pi
+        self.center = 5 * np.random.randn(2)
+        self.R = 5 + 5 * np.random.rand(1)
         self.t = np.stack(
             [
                 self.R * np.cos(angles) + self.center[0],
@@ -33,9 +33,9 @@ class TestCircle:
     def test_consistency(self, func_find_circle):
         center_x, center_y, radius = func_find_circle(self.t)
         # It is possible but highly improbable that this test fails by chance
-        assert pytest.approx(center_x, 1e-2) == self.center[0]
-        assert pytest.approx(center_y, 1e-2) == self.center[1]
-        assert pytest.approx(radius, 1e-2) == self.R
+        assert pytest.approx(center_x, 1e-4) == self.center[0]
+        assert pytest.approx(center_y, 1e-4) == self.center[1]
+        assert pytest.approx(radius, 1e-4) == self.R
 
     @pytest.mark.parametrize(
         "func_find_circle",
